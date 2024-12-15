e068483e6f223a377896da0d49047c56343c57bb
justo <119686375+justo5@users.noreply.github.com>
2024-12-15 15:34:39 -0300

    project initialized

---
+++ ojoDeBuey/src/main/java/com/example/ojoDeBuey/OjoDeBueyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ojoDeBuey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjoDeBueyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjoDeBueyApplication.class, args);
+	}
+
+}
---
+++ ojoDeBuey/src/test/java/com/example/ojoDeBuey/OjoDeBueyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ojoDeBuey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjoDeBueyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
